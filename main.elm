fc788af4d21665ea7ed416a4ad0a619c026cf681
Cspeisman <cspeisman@gmail.com>
2017-02-02 09:13:23 -0500

    bring animation frame to main module

--- main.elm
+++ main.elm
@@ -2,21 +2,30 @@ module Main exposing (..)
 
 import Html
 import Html.Attributes exposing (style)
+import Time exposing (Time, millisecond, second, every)
+import Animation exposing (..)
 import Mouse
 import Debug
+import AnimationFrame
 
 
 main =
     Html.program { init = ( model, Cmd.none ), view = view, update = update, subscriptions = subscriptions }
 
 
+type alias Model =
+    { clock : Time, activeAnimation : Maybe Animation, hello : List String }
+
+
 model =
-    [ ( "Hello World", { x = 0, y = 0 } ) ]
+    { clock = 0, hellos = [ ( "Hello World", { x = 0, y = 0 } ) ], activeAnimation = Nothing }
 
 
 type Msg
     = NoOp
+    | Tick Time
     | Click Mouse.Position
+    | Animate
 
 
 update msg model =
@@ -25,23 +34,51 @@ update msg model =
             ( model, Cmd.none )
 
         Click position ->
-            ( model ++ [ ( "Hello World", position ) ], Cmd.none )
+            ( { model | hellos = model.hellos ++ [ ( "Hello World", position ) ] }, Cmd.none )
+
+        Tick time ->
+            ( { model | clock = time }, Cmd.none )
+
+        Animate ->
+            ( { model | activeAnimation = Just (Animation.animation model.clock |> from 0 |> to 200) }, Cmd.none )
 
 
 subscriptions model =
-    Mouse.clicks Click
+    Sub.batch
+        [ Mouse.clicks Click
+        , AnimationFrame.times Tick
+        ]
 
 
 view model =
     let
+        x =
+            case model.activeAnimation of
+                Nothing ->
+                    0
+
+                Just a ->
+                    Animation.animate model.clock a
+
         elements =
-            List.map viewNode model
+            List.map (viewNode model.activeAnimation) model.hellos
     in
-        Html.div [] elements
+        Debug.log (toString x)
+            Html.div
+            []
+            elements
 
 
-viewNode node =
+viewNode activeAnimation node =
     let
+        x =
+            case activeAnimation of
+                Nothing ->
+                    0
+
+                Just a ->
+                    Animation.animate model.clock a
+
         ( text, coords ) =
             node
 
